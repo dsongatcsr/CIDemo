3f2f19ca599210fd45a3d75928d49716316b576d
dsongatcsr <dsong@csr.com.au>
2014-12-31 11:57:30 +1100

    init

---
+++ AzureDemoWeb/default.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AzureDemoWeb
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label1.Text = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ssZ");
+        }
+    }
+}
